523b2cd4e3418773a89b78b7bf2f6ee46dac0a0e
Bhilhanan A Jeyaram <bhilhanan@gmail.com>
2015-02-26 18:09:01 -0500

    Revert "pushing in another repository"
    This reverts commit 5015d7be16f7560bb389505d227ec248e56f7a7d.

---
+++ Multi threading/OS-Restaurant/src/DinerProperties.java
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * @author Bhilhanan A Jeyaram
+ *
+ */
+public class DinerProperties {
+	public Diner diner;
+	public Integer table;
+	public int foodStartTime;
+
+	public DinerProperties() {
+	}
+}
